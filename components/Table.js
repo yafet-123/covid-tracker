05680183e189b82d678e40b347daacfa2b72641e
yafet-123 <yafetaddisu123@gmail.com>
2022-09-30 08:26:04 +0300

    fixing bug

--- components/Table.js
+++ components/Table.js
@@ -3,8 +3,8 @@ import numeral from "numeral";
 function Table({ countries }) {
     return (
         <div className="table">
-            {countries.map((country) => (
-                <tr>
+            {countries.map((country, index) => (
+                <tr key={index}>
                     <td>{country.country}</td>
                     <td>
                         <strong>{numeral(country.cases).format("0,0")}</strong>
